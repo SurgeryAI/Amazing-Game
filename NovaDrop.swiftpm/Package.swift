d8b65c4c00e6a2f7a6d4dbf72d79b7d71aa36ed8
Marc <mmelcher@gmail.com>
2026-05-14 13:26:14 -0700

    updatea again

--- NovaDrop.swiftpm/Package.swift
+++ NovaDrop.swiftpm/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "com.bookmarc.gravitycascade",
             teamIdentifier: "C3U5S48E88",
-            displayVersion: "1.01",
+            displayVersion: "1.02",
             bundleVersion: "1",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.indigo),
